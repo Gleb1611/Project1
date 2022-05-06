f09d748fd457314e65fca0bf9f9aa18d36e1e3fe
Gleb <102374436+Gleb1611@users.noreply.github.com>
2022-05-06 20:10:28 +0300

    Update Main.cpp
    Testing polimorfizm.

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -3,14 +3,25 @@
 #include "Manager.h" 
 #include "Leader.h"
 
-void testing(Student* st)
-{
-	cout << st->getInfo() << endl;
+void testing(Student** list, int size) {
+	for (int i = 0; i < size; i++)
+	{
+		cout << list[i]->getInfo() << endl;
+	}
 }
 
+
 int main() {
-	Student* st = new Student("Alex", 15, 10, 'm');
-	testing(st);
+	const int SIZE = 2;
+
+	//Student* student = new Student[SIZE];
+
+	Student** list = new Student * [SIZE];
+
+	list[0] = new Student("Alex", 15, 10, 'm');
+	list[1] = new Leader("Nikita", 15, 4, 'f', "+375292655678", "TROLOLO@gmail.com");
+
+	testing(list, SIZE);
 
 	return 0;
 }
